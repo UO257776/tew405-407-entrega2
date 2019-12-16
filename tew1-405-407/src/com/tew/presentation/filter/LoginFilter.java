e516ab86a9fa124a834c7a352046948b42ab2c4b
David <uo246696@uniovi.es>
2019-12-16 05:10:34 +0100

    chain.doFilter(request, response) ya no está comentado

--- tew1-405-407/src/com/tew/presentation/filter/LoginFilter.java
+++ tew1-405-407/src/com/tew/presentation/filter/LoginFilter.java
@@ -57,7 +57,7 @@ public class LoginFilter implements Filter {
 	    	//res.sendRedirect(req.getContextPath() + loginForm);
 	    //} else {
 			// pass the request along the filter chain
-			//chain.doFilter(request, response);
+			chain.doFilter(request, response);
 	    //}
 	    
 	    //String usuario = request.getParameter("user");
